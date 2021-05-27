843b5436ffbb824a500d8b20a6d52d4605802e77
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2021-05-27 17:17:01 +0200

    Added Update post functionality

--- api/post/update.php
+++ api/post/update.php
@@ -2,13 +2,13 @@
 /**
  * Author: Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
  * Date: 5/26/2021
- * Time: 3:00 PM
+ * Time: 7:00 PM
  */
 
 // Headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=UTF-8');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: PUT');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 require_once '../../config/Database.php';
@@ -25,19 +25,19 @@ $post = new Post($db);
 $data = json_decode(file_get_contents("php://input"));
 
 try {
-    if (isset($data->title, $data->body, $data->author) && !empty($data->title) && !empty($data->body) && !empty($data->author)) {
-        $status = $post->create($data->title, $data->body, $data->author);
+    if (isset($data->id, $data->title, $data->body, $data->author) && !empty($data->title) && !empty($data->body) && !empty($data->author)) {
+        $status = $post->update($data->id, $data->title, $data->body, $data->author);
     } else {
         throw new Exception("Posts params not set :(");
     }
 
     if ($status) {
         echo json_encode(
-            array('status' => 'success', 'message' => 'Post Created Successfully :)')
+            array('status' => 'success', 'message' => 'Post Updated Successfully :)')
         );
     } else {
         echo json_encode(
-            array('status' => 'failed', 'message' => 'Error: failed to create the post :( , please try again')
+            array('status' => 'failed', 'message' => 'Error: Post not found :(')
         );
     }
 } catch (Exception $e) {
--- models/Post.php
+++ models/Post.php
@@ -39,15 +39,44 @@ class Post {
         // Prepare statement
         $stmt = $this->conn->prepare($sql);
         // Bind data
-        $stmt->bindParam(':title', $title, PDO::PARAM_INT);
+        $stmt->bindParam(':title', $title, PDO::PARAM_STR);
         $stmt->bindParam(':body', $body, PDO::PARAM_STR);
         $stmt->bindParam(':author', $author, PDO::PARAM_STR);
 
         // Execute query
         try {
             $stmt->execute();
+
             return true;
         } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    // Update post
+    public function update($id, $title, $body, $author)
+    {
+        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+        $title = htmlentities(strip_tags($title), ENT_QUOTES, 'UTF-8');
+        $body = htmlentities(strip_tags($body), ENT_QUOTES, 'UTF-8');
+        $author = htmlentities(strip_tags($author), ENT_QUOTES, 'UTF-8');
+
+        // Create query
+        $sql = "UPDATE `posts` SET `title` = :title, `body` = :body, `author` = :author WHERE `id` = :id";
+        // Prepare statement
+        $stmt = $this->conn->prepare($sql);
+        // Bind data
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+        $stmt->bindParam(':body', $body, PDO::PARAM_STR);
+        $stmt->bindParam(':author', $author, PDO::PARAM_STR);
+
+        // Execute query
+        $stmt->execute();
+
+        if ($stmt->rowCount()) {
+            return true;
+        } else {
             return false;
         }
     }
