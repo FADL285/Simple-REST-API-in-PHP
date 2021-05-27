be7a760068e3819ec3c8ba8c6cd97e35f0a4f2b4
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2021-05-27 20:58:59 +0200

    update allowed method in delete.php file

--- api/post/delete.php
+++ api/post/delete.php
@@ -8,7 +8,7 @@
 // Headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=UTF-8');
-header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Methods: DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 require_once '../../config/Database.php';
